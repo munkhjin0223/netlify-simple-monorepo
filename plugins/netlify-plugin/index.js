0cb383f2c01c88cfa6ee5363056c0f5e808a2e03
Munkhjin <munkhjin0223@gmail.com>
2020-06-25 12:40:32 +0800

    update

--- plugins/netlify-plugin/index.js
+++ plugins/netlify-plugin/index.js
@@ -6,7 +6,7 @@ module.exports = {
 
     console.log(process.env.SITE_ID, ' ----- ', constants.SITE_ID);
 
-    if (process.env.SITE_ID !== constants.SITE_ID) {
+    if (process.env.SITE_ID === constants.SITE_ID) {
       return utils.build.cancelBuild('Cancel message');
     }
   },
