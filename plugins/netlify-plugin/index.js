dc044322ffc6cf1c9afa3470367ecdf02d6a6d8f
Munkhjin <munkhjin0223@gmail.com>
2020-06-25 12:54:04 +0800

    update

--- plugins/netlify-plugin/index.js
+++ plugins/netlify-plugin/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   onPreBuild: (args) => {
-    console.log('-------args----------: ', args);
+    console.log('-------args----------: ', JSON.stringify(args));
 
     const { utils, constants } = args;
 
