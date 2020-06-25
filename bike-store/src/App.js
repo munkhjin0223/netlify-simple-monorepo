31d472acd30c5d50dc9c05d9fa63b1cdb8ff7555
Munkhjin <munkhjin0223@gmail.com>
2020-06-25 14:37:06 +0800

    change

--- bike-store/src/App.js
+++ bike-store/src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the Bike Store</p>
+        <p>Welcome to the Bike</p>
         <a
           className="App-link"
           href="https://reactjs.org"
